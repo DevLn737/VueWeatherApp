8154738077ca7d2d2f417883266591702e90e192
DevLn737 <esprgv@gmail.com>
2023-10-31 19:10:17 +0400

    frontend basic setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 //Импорты
 const express = require('express');
+const cors = require('cors')
 require('./db/db.js');
 
 // Роутеры
@@ -10,6 +11,9 @@ const weatherRouter = require('./routes/weather');
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+
+// Разрешить все источники (в продакшене изменить)
+app.use(cors());
 
 // Инициализация роутеров
 app.use(logsRouter);
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,61 @@
 <template>
-   <h1>Hello World</h1>
+      <main>
+         <div class="search-box">
+            <input
+               type="text"
+               class="seacth-bar"
+               placeholder="Enter the city..."
+               v-model="enteredCity"
+            />
+         </div>
+
+         <button type="button" @click="getWeather">Get weather</button>
+         {{ weather }}
+      </main>
 </template>
 
-<script></script>
+<script>
+export default {
+   data() {
+      return {
+         enteredCity: '',
+         weather: {},
+         isLoading: false,
+      };
+   },
+   methods: {
+      //WIP
+      async getWeather(city) {
+         try {
+            const response = await fetch(
+               `http://localhost:3000/v1/weather/${this.enteredCity}`
+            );
+            if (response.ok) {
+               this.weather = await response.json();
+            }
+         } catch (error) {
+            console.log(error);
+         }
+      },
+   },
+   // mounted() {
+   //    this.getWeather();
+   // },
+};
+</script>
+
+<style>
+* {
+   box-sizing: border-box;
+}
+html {
+   font-family: 'DM Sans', sans-serif;
+}
+body {
+   padding: 0;
+   margin: 0;
+}
+ul {
+   list-style: none;
+}
+</style>
